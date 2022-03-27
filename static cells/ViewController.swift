0f58115499c7384b02d244847cfad5bd30a95569
Huscro <apologize9100@gmail.com>
2022-03-28 02:39:41 +0800

    Initial Commit

---
+++ static cells/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  static cells
+//
+//  Created by 曾曜澤 on 2022/3/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
